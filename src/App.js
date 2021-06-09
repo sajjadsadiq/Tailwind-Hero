62138609227b6e3fa6844dadc494e931765b49b5
sajjad sadiq <sajjadsadiq2020@gmail.com>
2021-06-09 21:32:47 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tailwind Learn</h1>
+      <p>Powerd By <strong>Sajjad Sadiq</strong></p>
+    </div>
+  );
+}
+
+export default App;
