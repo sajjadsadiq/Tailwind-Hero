4e584ef0dc67e2c6da65824169acf0744d07def7
sajjad sadiq <sajjadsadiq2020@gmail.com>
2021-06-11 00:50:18 +0600

    update artical data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Tailwind Learn</h1>
       <p>Powerd By <strong>Sajjad Sadiq</strong></p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis laborum officia esse repellendus vel odio deleniti debitis nesciunt numquam quia, itaque non? Repellendus, qui eius rerum beatae doloribus neque incidunt?</p>
     </div>
   );
 }
